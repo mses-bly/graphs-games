0763a1db529c015d250f53ab1674913f613dd832
Moises Baly <moises.baly@gmail.com>
2016-11-26 14:13:11 -0500

    About to break some stuff

--- src/graph/MarketFrame.java
+++ src/graph/MarketFrame.java
@@ -3,6 +3,8 @@ package graph;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 import static graph.GraphOps.bfs;
@@ -52,6 +54,173 @@ public class MarketFrame {
 			it = matchingIteration(residual, 0, residual.size() - 1);
 		}
 		return m;
+	}
+
+	/**
+	 * Finds the market equilibrium: the prices for the goods being sold.
+	 *
+	 * @return
+	 */
+	public static HashMap<Integer, Double> findMarketEquilibriumPrices(Graph market, HashMap<Integer, Double> initialPrices) {
+		return findOptimalOutcome(market, initialPrices).sellerPrices;
+	}
+
+	/**
+	 * Finds the market equilibrium: the optimal matches between the buyers and the goods
+	 *
+	 * @return
+	 */
+	public static Matching findMarketEquilibriumMatching(Graph market, HashMap<Integer, Double> initialPrices) {
+		return findOptimalOutcome(market, initialPrices).matching;
+	}
+
+	/**
+	 * Applies VCG mechanism to find prices paid by buyers.
+	 *
+	 * @param market
+	 * @param initialPrices maybe the market has prices that are not optimal yet. Iterate from them.
+	 * @return
+	 */
+	public static HashMap<Integer, Double> VCG(Graph market, HashMap<Integer, Double> initialPrices) {
+		Outcome optimal = findOptimalOutcome(market, initialPrices);
+		Matching optimalMatching = optimal.matching;
+		double sum = 0;
+		HashMap<Integer, Double> p_i = new HashMap<>();
+
+		for (int i = 0; i < optimalMatching.matches.size(); i++) {
+			Matching.Match m = optimalMatching.matches.get(i);
+			double value = market.findAdjacentNode(m.i, m.j).c;
+			p_i.put(m.i, value);
+			sum += value;
+		}
+
+		for (int key : p_i.keySet()) {
+			p_i.put(key, -(sum - p_i.get(key)));
+		}
+		return p_i;
+	}
+
+	/**
+	 * Implementation of VCG with Clark pivot.
+	 *
+	 * @param market
+	 * @param initialPrices
+	 * @return
+	 */
+	public static HashMap<Integer, Double> VCGClark(Graph market, HashMap<Integer, Double> initialPrices) {
+		HashMap<Integer, Double> optimal_pi = VCG(market, initialPrices);
+		HashMap<Integer, Double> clark_p_i = new HashMap<>();
+		// Get the buyers from our artificial source
+		for (Node n : market.adjacents(0)) {
+			Graph temp = new Graph(market);
+			// Disconnect current node to compute social value when not present.
+			temp.disconnectNode(n.ID);
+			Outcome optimal = findOptimalOutcome(temp, initialPrices);
+			double social_value = 0;
+			for (Matching.Match m : optimal.matching.matches) {
+				social_value += temp.findAdjacentNode(m.i, m.j).c;
+			}
+
+			clark_p_i.put(n.ID, social_value + optimal_pi.get(n.ID));
+		}
+		return clark_p_i;
+	}
+
+
+	/**
+	 * An outcome of applying a particular mechanisms to a market frame
+	 */
+	private static class Outcome {
+		private HashMap<Integer, Double> sellerPrices;
+		private Matching matching;
+
+		public Outcome(HashMap<Integer, Double> sellerPrices, Matching matching) {
+			this.sellerPrices = sellerPrices;
+			this.matching = matching;
+		}
+	}
+
+	/**
+	 * Finds the preferred sellers based on buyers values and sellers prices (utility maximization)
+	 *
+	 * @param market
+	 * @param prices
+	 * @return
+	 */
+	private static Graph preferredSellerGraph(Graph market, HashMap<Integer, Double> prices) {
+		// Create a copy of the graph.
+		Graph preferredG = new Graph(market);
+		// Get the buyers using our artificial source node.
+		ArrayList<Node> buyers = market.adjacents(0);
+		// Update the graph by removing the non-preferred sellers
+		for (Node buyer : buyers) {
+			double maxUtility = Double.MIN_VALUE;
+			int preferredSeller = -1;
+			// For every seller, figure out which one is the one we want.
+			for (Node seller : market.adjacents(buyer.ID)) {
+				// find the utility of this bid.
+				double utility = seller.c - prices.get(seller.ID);
+				if (utility >= maxUtility) {
+					maxUtility = utility;
+					preferredSeller = seller.ID;
+				}
+			}
+			// Remove all bids except the preferred seller
+			preferredG.removeExceptUnd(buyer.ID, preferredSeller);
+		}
+		return preferredG;
+	}
+
+	/**
+	 * In a bipartite graph, computes how many buyers have placed at least one bid to
+	 * the sellers/items.
+	 *
+	 * @param bipartite
+	 * @return
+	 */
+	private static int bipartitePairs(Graph bipartite) {
+		int pairs = 0;
+		// Get left component from our artificial source node.
+		for (Node n : bipartite.adjacents(0)) {
+			if (!bipartite.adjacents(n.ID).isEmpty()) pairs++;
+		}
+		return pairs;
+	}
+
+	/**
+	 * Computes the social optimum in a market starting with some seller prices.
+	 *
+	 * @param market
+	 * @param prices
+	 * @return the optimum outcome
+	 */
+	private static Outcome findOptimalOutcome(Graph market, HashMap<Integer, Double> prices) {
+		// There can't be market equilibrium.
+		if (market.size() % 2 != 0) return null;
+		HashMap<Integer, Double> tmpPrices = new HashMap<>(prices);
+		Graph preferredGraph = preferredSellerGraph(market, tmpPrices);
+		Matching matching = maxMatching(preferredGraph);
+		while (!matching.isMaximum(bipartitePairs(preferredGraph))) {
+			preferredGraph.print();
+			for (Matching.Match m : matching.matches) {
+				tmpPrices.put(m.j, tmpPrices.get(m.j) + 1);
+			}
+			boolean gtZero = true;
+			for (Map.Entry<Integer, Double> entry : tmpPrices.entrySet()) {
+				if (entry.getValue() <= 0) {
+					gtZero = false;
+					break;
+				}
+			}
+			if (gtZero) {
+				for (int key : tmpPrices.keySet()) {
+					tmpPrices.put(key, tmpPrices.get(key) - 1);
+				}
+			}
+			preferredGraph = preferredSellerGraph(market, tmpPrices);
+			matching = maxMatching(preferredGraph);
+		}
+		return new Outcome(tmpPrices, matching);
 	}
 
 	/**
--- src/hm/HM3.java
+++ src/hm/HM3.java
@@ -3,8 +3,14 @@ package hm;
 import graph.Graph;
 import graph.Market;
 import graph.MarketFrame;
+import graph.Matching;
 
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.*;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Scanner;
 
 /**
  * Created by moises on 11/23/16.
@@ -12,32 +18,119 @@ import java.io.FileNotFoundException;
 public class HM3 {
 	private static void run_10_a() throws FileNotFoundException {
 		// Construct the market graph.
-		Graph g = MarketFrame.constructMarket(System.getProperty("user.dir") + "/src/hm/market_7.3.txt");
+		Graph g = MarketFrame.constructMarket(System.getProperty("user.dir") + "/src/hm/random_bipartite.txt");
 		MarketFrame.maxMatching(g);
-		System.out.println(MarketFrame.maxMatching(g));
+		Matching m = MarketFrame.maxMatching(g);
+		System.out.println("Executing maximum matching in `random_bipartite.txt`");
+		System.out.println("Matchings: " + m);
+		System.out.println("Matching size: " + m.size());
+		System.out.println("Is Maximum: " + m.isMaximum(6));
+
+		// Remove a few edges and find a constrained set.
+		g.removeEdge(5, 10);
+		g.removeEdge(10, 5);
+		m = MarketFrame.maxMatching(g);
+		System.out.println("Matchings: " + m);
+		System.out.println("Matching size: " + m.size());
+		System.out.println("Is Maximum: " + m.isMaximum(6));
 
 
 	}
 
+	private static void run_10_b() throws FileNotFoundException {
+		// Construct the market graph.
+		Graph g = MarketFrame.constructMarket(System.getProperty("user.dir") + "/src/hm/market_7_3.txt");
+		HashMap<Integer, Double> initialPrices = new HashMap<Integer, Double>();
+//		initialPrices.put(4, 0.0);
+//		initialPrices.put(5, 0.0);
+//		initialPrices.put(6, 0.0);
+//		System.out.println("Finding equilibrium in market `market_7_3.txt`");
+//		System.out.println("Equilibrium prices: " + MarketFrame.findMarketEquilibriumPrices(g, initialPrices));
+//		System.out.println("Equilibrium matchings: " + MarketFrame.findMarketEquilibriumMatching(g, initialPrices));
+
+
+		g = MarketFrame.constructMarket(System.getProperty("user.dir") + "/src/hm/market_advertisers.txt");
+		initialPrices = new HashMap<Integer, Double>();
+		initialPrices.put(5, 0.0);
+		initialPrices.put(6, 0.0);
+		initialPrices.put(7, 0.0);
+		initialPrices.put(8, 0.0);
+		System.out.println("Finding equilibrium in market `market_advertisers.txt`");
+		System.out.println("Equilibrium prices: " + MarketFrame.findMarketEquilibriumPrices(g, initialPrices));
+//		System.out.println("Equilibrium matchings: " + MarketFrame.findMarketEquilibriumMatching(g, initialPrices));
+
+	}
+
+	private static void run_11_b() throws FileNotFoundException {
+		// Construct the market graph.
+		Graph g = MarketFrame.constructMarket(System.getProperty("user.dir") + "/src/hm/market_7_3.txt");
+		HashMap<Integer, Double> initialPrices = new HashMap<Integer, Double>();
+		initialPrices.put(4, 0.0);
+		initialPrices.put(5, 0.0);
+		initialPrices.put(6, 0.0);
+		System.out.println("Applying VCG in market `market_7_3.txt`");
+		System.out.println("Buyers prices to pay: " + MarketFrame.VCG(g, initialPrices));
+
+
+		g = MarketFrame.constructMarket(System.getProperty("user.dir") + "/src/hm/market_advertisers.txt");
+		initialPrices = new HashMap<Integer, Double>();
+		initialPrices.put(5, 0.0);
+		initialPrices.put(6, 0.0);
+		initialPrices.put(7, 0.0);
+		initialPrices.put(8, 0.0);
+		System.out.println("Applying VCG in market `market_advertisers.txt`");
+		System.out.println("Buyers prices to pay: " + MarketFrame.VCG(g, initialPrices));
+
+	}
+
+	private static void run_11_c() throws FileNotFoundException {
+		// Construct the market graph.
+		Graph g = MarketFrame.constructMarket(System.getProperty("user.dir") + "/src/hm/market_7_3.txt");
+		HashMap<Integer, Double> initialPrices = new HashMap<Integer, Double>();
+		initialPrices.put(4, 0.0);
+		initialPrices.put(5, 0.0);
+		initialPrices.put(6, 0.0);
+		System.out.println("Applying VCG in market `market_7_3.txt`");
+		System.out.println("Buyers prices to pay: " + MarketFrame.VCGClark(g, initialPrices));
+
+
+		g = MarketFrame.constructMarket(System.getProperty("user.dir") + "/src/hm/market_advertisers.txt");
+		initialPrices = new HashMap<Integer, Double>();
+		initialPrices.put(5, 0.0);
+		initialPrices.put(6, 0.0);
+		initialPrices.put(7, 0.0);
+		initialPrices.put(8, 0.0);
+		System.out.println("Applying VCG in market `market_advertisers.txt`");
+		System.out.println("Buyers prices to pay: " + MarketFrame.VCGClark(g, initialPrices));
+
+	}
+
+
+	private static Graph run_12_a() {
+		int buyers = 20;
+		int items = 20;
+		Graph gr = new Graph(buyers + items + 2);
+		// Creating artificial source node.
+		for (int i = 1; i <= buyers; i++) {
+			gr.connect(0, i, 1, 1);
+		}
+		// Creating artificial sink node.
+		for (int i = 21; i <= buyers + items; i++) {
+			gr.connect(i, 41, 1, 1);
+		}
+		Random r = new Random(System.currentTimeMillis());
+		for (int buyer = 1; buyer <= buyers; buyer++) {
+			for (int item = 21; item <= buyers + items; item++) {
+				// buyer will give us the number of items in the bundle.
+				gr.connectUnd(buyer, item, 1, (item - 20) * (r.nextInt(51)));
+			}
+		}
+		return gr;
+	}
+
+
 	public static void main(String[] args) throws FileNotFoundException {
-		run_10_a();
-
-
-//		Graph g = hm3.constructWithCapacity(System.getProperty("user.dir") + "/src/hm/bipartite_1.txt");
-//		g.print();
-//
-//		Matching m = BiPartiteOps.maxMatching(g);
-//		System.out.println(m);
-
-
-//		Market m = hm3.constructMarket(System.getProperty("user.dir") + "/src/hm/market_7.3.txt");
-//		System.out.println(m.findMarketEquilibrium());
-//		System.out.println("Done");
-//
-//		Market m = hm3.constructMarket(System.getProperty("user.dir") + "/src/hm/market_7.3.txt");
-
-//		System.out.println(m.VCGClark());
-
+		run_10_b();
 	}
 
 }
